f9377df2eec21fb08c69545efd088998ccb8e369
Udhay2608 <U-Tiwana23@st-bernards.slough.sch.uk>
2026-01-02 22:27:12 +0000

    Add files via upload

---
+++ key word hw .py
@@ -0,0 +1,10 @@
+# Total bill and amount paid
+total_bill = 2.50
+amount_paid = 4.00
+
+# Calculate change
+change = amount_paid - total_bill
+
+print("Total bill: $", total_bill)
+print("Amount paid: $", amount_paid)
+print("Change to return: $", round(change, 2))
